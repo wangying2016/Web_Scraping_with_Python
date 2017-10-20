df1478059667a3d3766d5434facfe290a7678558
Wang Ying <wy867504146@outlook.com>
2017-10-20 19:29:23 +0800

    Chapter1 Codes

--- Chapter1/scrapetest.py
+++ Chapter1/scrapetest.py
@@ -1,4 +1,23 @@
-
 from urllib.request import urlopen
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-print(html.read())
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        print(e)
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read())
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+title = getTitle("http://www.pythonscraping.com/exercises/exercise1.html")
+if title == None:
+    print("Title could not be found")
+else:
+    print(title)
