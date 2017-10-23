8ebd93431cdcef3449fdf25ed5042062a0872d79
Wang Ying <wy867504146@outlook.com>
2017-10-23 20:59:25 +0800

    Chapter 2 select by images

--- Chapter2/selectByImages.py
+++ Chapter2/selectByImages.py
@@ -1,9 +1,10 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from bs4 import re
 
 
 html = urlopen("http://www.pythonscraping.com/pages/page3.html")
 bsObj = BeautifulSoup(html)
-print(bsObj.find("img", {"src": "../img/gifts/img1.jpg"
-                         }).parent.previous_sibling.get_text())
-
+images = bsObj.findAll("img", {"src": re.compile("\.\./img/gifts/img.*\.jpg")})
+for image in images:
+    print(image["src"])
